89cd56cb048824d66660995a3aa9eea12ae1fabe
mojtaba <mojtabah@buffalo.edu>
2016-04-27 22:17:38 -0400

    debugged nn_dsgd and now it's ready to be used

--- cheml/nn/nn_dsgd.py
+++ cheml/nn/nn_dsgd.py
@@ -6,13 +6,13 @@ import nn_psgd
 from ..utils import chunk
 
 def nn_dsgd_train(X,Y,nneurons,input_act_funcs,validation=0.1,learn_rate=0.001,rms_decay=0.9,n_epochs=10000,
-    batch_size=256,n_cores=1,n_hist=20,n_check=50,threshold=0.1, print_level=1):
+    batch_size=256,n_hist=20,n_check=50,threshold=0.1, print_level=1):
     """
     Main distributed memory function
     
     Parameters
     ----------
-    All available parameters for nn_psgd + validation
+    All available parameters for nn_psgd - n_cores + validation
     
     validation: float between zero and one, optional(default = 0.1)
         The ratio of data set to be used for validation of trained network. The 
@@ -40,18 +40,28 @@ def nn_dsgd_train(X,Y,nneurons,input_act_funcs,validation=0.1,learn_rate=0.001,r
             indices = []
             for j in range(c):
                 indices+=chunk_list.pop()
-            comm.send(indices,dest=i)
-    
-    indices = comm.recv(source=0)
-    cut_ind = int(validation * len(indices))
-    X_validation = X[indices[:cut_ind]]
-    Y_validation = Y[indices[:cut_ind]]
-    X = X[indices[cut_ind:]]
-    Y = Y[indices[cut_ind:]]
+            if i!=0:
+                comm.send(X[indices],dest=i, tag = 7)
+                comm.send(Y[indices],dest=i, tag = 77)
+            else:
+                Xnew = X[indices]
+                Ynew = Y[indices]
+        X = Xnew
+        Y = Ynew
+    else:
+        X = comm.recv(source=0, tag = 7)
+        Y = comm.recv(source=0, tag = 77)
+
+    cut_ind = int(validation * len(X))
+    X_validation = X[:cut_ind]
+    Y_validation = Y[:cut_ind]
+    X = X[cut_ind:]
+    Y = Y[cut_ind:]
     trained_network =  nn_psgd.train(X,X_validation,Y,Y_validation,nneurons=nneurons,
     input_act_funcs=input_act_funcs,learn_rate=learn_rate,rms_decay=rms_decay,
-    n_epochs=n_epochs,batch_size=batch_size,n_cores=n_cores,n_hist=n_hist,
+    n_epochs=n_epochs,batch_size=batch_size,n_cores=multiprocessing.cpu_count(),n_hist=n_hist,
     n_check=n_check,threshold=threshold, print_level=print_level)
+    
     trained_network = comm.gather(trained_network,root=0)
     if rank==0:
         return trained_network
@@ -72,6 +82,13 @@ def nn_dsgd_output(X,nnets):
     -------
     predicted values in array type
     """
+    #MPI
+    comm=MPI.COMM_WORLD
+    rank=comm.rank
+    size=comm.size
+    
     if rank == 0:
+        results = []
         for nn in nnets:
-            return nn_psgd._output(X,nn['weights'],nn_psgd.act_funcs_from_string(nn['act_funcs'],len(nn['weights'])-1))
+            results+= [nn_psgd._output(X,nn['weights'],nn_psgd.act_funcs_from_string(nn['act_funcs'],len(nn['weights'])-1))]
+        return results

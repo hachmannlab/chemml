e2a63526a73fa281a24b24975828d3573d2c0f44
mojtaba <mojtabah@buffalo.edu>
2017-01-17 07:26:27 -0500

    debuged and tested

--- cheml/nn/tensorflow/__init__.py
+++ cheml/nn/tensorflow/__init__.py
@@ -3,7 +3,8 @@ The 'cheml.nn' module includes neural_net,
 last modified date: April 24, 2016
 """
 
-from .mlp_classification import mlp_classification
+from .mlp import mlp_classification
+from .mlp import mlp_regression
 # from .nn_SGD_Distributed import nn_dsgd_train, nn_dsgd_output
 #Zfrom .nn_SGD_tensorflow import nn_tf
 #from .nn_SGD_theano import nn_theano
@@ -11,8 +12,8 @@ from .mlp_classification import mlp_classification
 
 
 __all__ = [
-    'multilayer_perceptron',
-    'recurrent_network',
+    'mlp_classification',
+    'mlp_regression',
     'convolutional',
     '',
 ]

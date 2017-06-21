0352150576569fd160780b8f73f4f5d4d5c99e7c
mojtaba <mojtabah@buffalo.edu>
2017-06-21 13:29:37 -0400

    just for test

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from cheml import run
-run(INPUT_FILE = 'input_files/ri_prep_reg_eval.txt', OUTPUT_DIRECTORY = 'RI_NN_dragon_8:30_RI.out')
+run(INPUT_FILE = 'input_files/ri_data.txt', OUTPUT_DIRECTORY = 'a.out')
+# run(INPUT_FILE = 'input_files/ri_data.txt', OUTPUT_DIRECTORY = '../../RI_project/liq_org/1.out')
 
 

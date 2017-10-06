deb019e8e695c94ffd5187ed38406d74ea8b5c1e
mojtaba <mojtabah@buffalo.edu>
2017-10-05 23:57:22 -0400

    initiated cheml wrapper ui

---
+++ cheml/wrappers/ui/__init__.py
@@ -0,0 +1,9 @@
+"""
+The 'cheml.wrappers.ui' module provides a console user interface for the ChemML Wrapper, using Urwid library. 
+last modified date: October, 05, 2017
+"""
+
+# from test import test
+
+
+__all__ = []

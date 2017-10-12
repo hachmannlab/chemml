688953afd3b17a1f8b00e860840af7f82e52e2f9
mojtaba <mojtabah@buffalo.edu>
2017-10-12 17:15:52 -0400

    this module automatically reads the task,subtask,host,and function from database

---
+++ cheml/wrappers/database/TSHF.py
@@ -0,0 +1,29 @@
+import sklearn_db
+import cheml_db
+import inspect
+
+def tshf():
+    """
+    tshf stands for the combination of task, subtask, host, and function
+    :return: combination, dictionary of the aforementioned combinations
+    """
+    # 7 tasks
+    tasks = ['Enter', 'Prepare', 'Model', 'Search', 'Mix', 'Visualize', 'Store']
+    combination = {task: {} for task in tasks}
+    all_classes = [k[1] for k in inspect.getmembers(sklearn_db) if k[0][0:2]!='__']
+    all_classes += [k[1] for k in inspect.getmembers(cheml_db) if k[0][0:2]!='__' ]
+    for k in all_classes:
+        vk = vars(k)
+        if 'task' in vk and 'subtask' in vk:
+            task, subtask, host, function = [vk['task'], vk['subtask'], vk['host'], vk['function']]
+            if subtask not in combination[task]:
+                combination[task][subtask] = {host: [function]}
+            else:
+                if host not in combination[task][subtask]:
+                    combination[task][subtask][host] = [function]
+                else:
+                    combination[task][subtask][host].append(function)
+    return tasks, combination
+
+
+

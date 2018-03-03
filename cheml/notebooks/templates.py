55e50153d812d74a0f1a48398bf2d7ba6a200b7f
mojtaba <mojtabah@buffalo.edu>
2018-03-03 16:28:51 -0500

    added a tutorial and template2

--- cheml/notebooks/templates.py
+++ cheml/notebooks/templates.py
@@ -20,3 +20,73 @@ def template1():
 """
     return script.strip().split('\n')
 
+def template2():
+    script ="""
+
+        ## (Prepare,split)
+            << host = sklearn
+            << function = train_test_split
+            << test_size = 0.1
+            << random_state = 0
+            >> dfx_train 0
+            >> dfy_train 1
+
+        ## (Prepare,scale)
+            << host = sklearn
+            << function = StandardScaler
+            << func_method = fit_transform
+            >> 0 df
+            >> df 4
+
+        ## (Prepare,scale)
+            << host = sklearn
+            << function = StandardScaler
+            << func_method = fit_transform
+            >> 1 df
+            >> df 3
+
+        ## (Search,evaluate)
+            << host = sklearn
+            << function = scorer_regression
+            << greater_is_better = False
+            >> scorer 2
+
+        ## (Model,regression)
+            << host = sklearn
+            << function = MLPRegressor
+            << validation_fraction = 0.1
+            << early_stopping = True
+            >> api 5
+
+        ## (Search,grid)
+            << host = sklearn
+            << function = GridSearchCV
+            << scoring = @scorer
+            << estimator = @estimator
+            << param_grid = {'alpha':[3,1,.3,.1,.03,.01]}
+            << cv = 3
+            >> 2 scorer
+            >> 3 dfy
+            >> 4 dfx
+            >> 5 estimator
+            >> cv_results_ 6
+            >> cv_results_ 7
+
+        ## (Enter,python script)
+            << host = cheml
+            << function = PyScript
+            << line01 = print iv1.head(10)
+            >> 6 iv1
+
+        ## (Store,file)
+            << host = cheml
+            << function = SaveFile
+            << format = txt
+            << header = True
+            << filename = GridSearchCV_results
+            >> 7 df
+
+
+    """
+    return script.strip().split('\n')
+

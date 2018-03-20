4683200aac755c1b0d20fa61866411c2def542e2
Gaurav Vishwakarma <gvishwak@buffalo.edu>
2018-03-20 13:38:18 -0400

    updated init file

--- cheml/search/__init__.py
+++ cheml/search/__init__.py
@@ -3,7 +3,7 @@ The cheml.search module includes:
     - GA_DEAP: :func:`~cheml.search.GA_DEAP`
 """
 
-from ga_deap import GA_DEAP
+from genetic_algorithm import GA_DEAP
 
 __all__ = [
     'GA_DEAP',

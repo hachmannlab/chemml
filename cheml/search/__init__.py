94822a6024b02a5d9fccc2920aadbaeb0b546765
mojtaba <mojtabah@buffalo.edu>
2018-03-21 16:12:02 -0400

    added GA_DEAP

--- cheml/search/__init__.py
+++ cheml/search/__init__.py
@@ -3,7 +3,7 @@ The cheml.search module includes (please click on links adjacent to function nam
     - GA_DEAP: :func:`~cheml.search.GA_DEAP`
 """
 
-from genetic_algorithm import GA_DEAP
+from .genetic_algorithm import GA_DEAP
 
 __all__ = [
     'GA_DEAP',
--- cheml/search/genetic_algorithm.py
+++ cheml/search/genetic_algorithm.py
@@ -67,16 +67,9 @@ class GA_DEAP(object):
         Examples
         --------
         >>> from cheml.search import GA_DEAP
-        >>> Weights = (1,)
-        >>> chromosome_length = 2
-        >>> chromosome_type = (1,1)
-        >>> bit_limits = ( (0,10), (0,5) )
-        >>> mut_int_lower = (0,0)
-        >>> mut_int_upper = (10,5)
-        >>> def Evaluate(individual):
-        >>>    return sum(individual),
-        >>> ga_search = GA_DEAP(Evaluate, chromosome_length = chromosome_length, chromosome_type = chromosome_type,
-        >>>       bit_limits = bit_limits, mut_int_lower=mut_int_lower, mut_int_upper=mut_int_upper )
+        >>> def sum_func(individual): return (sum(individual),)
+        >>> ga_search = GA_DEAP(Evaluate = sum_func, Weights = (1,), chromosome_length = 2, chromosome_type = (1,1),
+        >>>       bit_limits = ((0,10), (0,5)), mut_int_lower = (0,0), mut_int_upper = (10,5))
         >>> ga_search.fit()
         >>> best_ind_df, best_individual = ga_search.algorithm_1()
         Best Individuals of each generation are:

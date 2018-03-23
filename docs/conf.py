299dbe53a278f934c48765278d9ac4986003a85c
mojtaba <mojtabah@buffalo.edu>
2018-03-23 14:14:09 -0400

    rst_generator in place

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'ChemML'
-copyright = u'2016, Mojtaba Haghighatlari'
+copyright = u'2016, Mojtaba Haghighatlari and Hachmann lab'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

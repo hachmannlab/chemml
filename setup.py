eb42439e33a283050991a4c5d8c185250f44447c
mojtaba <mojtabah@buffalo.edu>
2018-03-23 14:17:29 -0400

    moved to the proper place

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import cheml
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md')) as f:
     long_description = f.read()
 
 setup(  name = 'chemml',
@@ -20,9 +20,9 @@ setup(  name = 'chemml',
         description='A Machine Learning and Informatics Program Suite for the Chemical and Materials Sciences',
         long_description=long_description,
 
-        packages = find_packages(exclude=['docs', 'rst_generator']),
+        packages = find_packages(exclude=['docs']),
 
-        py_modules = ['chemmlwrapper'],
+        scripts = ['bin/chemmlwrapper'],
 
         keywords=['Machine Learning', 'Data Mining', 'Quantum Chemistry', 'Materials Science', 'Informatics'],
         license='3-Clause BSD',
@@ -32,17 +32,25 @@ setup(  name = 'chemml',
                    'License :: OSI Approved :: 3-Clause BSD License',
                   ],
 
-        python_requires = '==2.7',
+        python_requires = '>=2.7, <3',
 
-        install_requires = ['numpy>=1.13', 'pandas>=0.20.3', 'tensorflow>=1.4.1', 'keras>=2.1.2',
-                            'scikit-learn>=0.18.1', 'babel>=2.3.4', 'matplotlib>=1.5.1', 'deap>=1.2.2',
+        install_requires = ['numpy>=1.13', 'pandas>=0.20.3', 'tensorflow==1.1.0', 'keras==2.1.5',
+                            'scikit-learn==0.19.1', 'babel>=2.3.4', 'matplotlib>=1.5.1', 'deap>=1.2.2',
                             'lxml','ipywidgets','graphviz'],
+
+        test_suite='nose.collector',
+        tests_require=['nose'],
+
+        include_package_data = True,
+        # package_data={
+        #                 '': ['*.xyz', '*.csv', '*.vasp', '*.txt'],
+        #                 # 'cheml': ['datasets/data/*', 'tests/data/*', 'tests/configfiles/*'],
+        #             },
         extras_require={
                 'gui': ['ipywidgets', 'graphviz'],
                 'ml': ['scikit-learn>=0.18.1','tensorflow>=1.4.1', 'keras>=2.1.2'],
                 'viz': ['matplotlib>=1.5.1'],
                 'ga': ['deap>=1.2.2'],
-
 
             },
      )

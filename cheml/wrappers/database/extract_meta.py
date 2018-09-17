76cc66d0cf76753e66b912ec3922a261225225a9
mojtaba <mojtabah@buffalo.edu>
2018-09-17 15:05:24 -0700

    removed for the sake of transition to ChemEco

--- cheml/wrappers/database/extract_meta.py
+++
@@ -1,9 +0,0 @@
-from cheml.wrappers.database.TSHF import tshf, get_complete_meta
-
-
-tasks, combination = tshf()
-print 'tasks : ', tasks
-print 'combination : ', combination
-
-meta = get_complete_meta()
-print 'Meta : ', meta

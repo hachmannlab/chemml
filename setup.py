31f78638d5686c887f642e913ddedc64510defcb
mojtaba <mojtabah@buffalo.edu>
2018-09-18 17:44:01 -0700

    added new requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from setuptools import setup, find_packages
+import setuptools
 from os import path
-import cheml
+import chemml
 
 here = path.abspath(path.dirname(__file__))
 
@@ -8,8 +8,10 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md')) as f:
     long_description = f.read()
 
-setup(  name = 'chemml',
-        version = cheml.__version__,
+if __name__ == "__main__":
+    setuptools.setup(
+        name='chemml',
+        version=chemml.__version__,
         author='Mojtaba Haghighatlari, Johannes Hachmann',
         author_email='mojtabah@buffalo.edu, hachmann@buffalo.edu',
         # url='https://github.com/hachmannlab/chemml',
@@ -17,37 +19,59 @@ setup(  name = 'chemml',
             'Source': 'https://github.com/hachmannlab/chemml',
             'url': 'https://hachmannlab.github.io/chemml/'
         },
-        description='A Machine Learning and Informatics Program Suite for the Chemical and Materials Sciences',
+        description=
+        'A Machine Learning and Informatics Program Suite for the Chemical and Materials Sciences',
         long_description=long_description,
+        keywords=[
+            'Machine Learning', 'Data Mining', 'Quantum Chemistry',
+            'Materials Science', 'Informatics'
+        ],
+        license='BSD-3C',
+        packages=setuptools.find_packages(),
+        scripts=['bin/chemeco'],
 
-        packages = find_packages(exclude=['docs']),
-
-        keywords=['Machine Learning', 'Data Mining', 'Quantum Chemistry', 'Materials Science', 'Informatics'],
-        license='3-Clause BSD',
-        classifiers=['Development Status :: 4 - Beta',
-                   'Natural Language :: English',
-                   'Programming Language :: Python :: 2.7',
-                  ],
-
-        python_requires = '>=2.7, <3',
-
-        install_requires = ['numpy>=1.13', 'pandas>=0.20.3', 'tensorflow==1.1.0', 'keras==2.1.5',
-                            'scikit-learn==0.19.1', 'babel>=2.3.4', 'matplotlib>=1.5.1', 'deap>=1.2.2',
-                            'lxml','nose','ipywidgets>=7.1','widgetsnbextension>=3.1','graphviz'],
-
-        test_suite='nose.collector',
-        tests_require=['nose'],
-
-        include_package_data = True,
-        # package_data={
-        #                 '': ['*.xyz', '*.csv', '*.vasp', '*.txt'],
-        #                 # 'cheml': ['datasets/data/*', 'tests/data/*', 'tests/configfiles/*'],
-        #             },
+        # python_requires='>=2.7, <3',
+        install_requires=[
+            'future', 'six', 'numpy', 'pandas', 'tensorflow==1.1.0',
+            'keras==2.1.5', 'scikit-learn==0.19.1', 'babel>=2.3.4',
+            'matplotlib>=1.5.1', 'deap>=1.2.2', 'lxml', 'graphviz'
+        ],
         extras_require={
-                'gui': ['ipywidgets', 'graphviz'],
-                'ml': ['scikit-learn>=0.18.1','tensorflow>=1.4.1', 'keras>=2.1.2'],
-                'viz': ['matplotlib>=1.5.1'],
-                'ga': ['deap>=1.2.2'],
+            'docs': [
+                'sphinx',
+                'sphinxcontrib-napoleon',
+                'sphinx_rtd_theme',
+                'numpydoc',
+            ],
+            'tests': [
+                'pytest',
+                'pytest-cov',
+                'pytest-pep8',
+                'tox',
+            ],
+        },
+        tests_require=[
+            'pytest',
+            'pytest-cov',
+            'pytest-pep8',
+            'tox',
+        ],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Natural Language :: English',
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.5',
+        ],
+        zip_safe=False,
+    )
 
-            },
-     )
+    # install_requires = ['numpy>=1.13', 'pandas>=0.20.3', 'tensorflow==1.1.0', 'keras==2.1.5',
+    #                     'scikit-learn==0.19.1', 'babel>=2.3.4', 'matplotlib>=1.5.1', 'deap>=1.2.2',
+    #                     'lxml','nose','ipywidgets>=7.1','widgetsnbextension>=3.1','graphviz'],
+
+    # include_package_data = True,
+    # package_data={
+    #                 '': ['*.xyz', '*.csv', '*.vasp', '*.txt'],
+    #                 # 'cheml': ['datasets/data/*', 'tests/data/*', 'tests/configfiles/*'],
+    #             },

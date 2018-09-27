51e7f0baf4412be45a5b898224f8b7063b444a29
mojtaba <mojtabah@buffalo.edu>
2018-09-27 12:18:02 -0700

    convert nose to pytest

--- tests/test_XYZreader.py
+++ tests/test_XYZreader.py
@@ -1,36 +1,29 @@
-import unittest
+import pytest
 import os
 import pkg_resources
 
-from cheml.initialization import XYZreader
+from chemml.initialization import XYZreader
 
-DATA_PATH = pkg_resources.resource_filename('cheml', os.path.join('tests', 'data'))
+@pytest.fixture()
+def data_path():
+    return pkg_resources.resource_filename('chemml', os.path.join('datasets','data','organic_xyz'))
 
-print 'path:', DATA_PATH
+def test_string_manual(data_path):
+    reader= XYZreader(path_pattern='[2-3]_opt.xyz',
+                                       path_root = data_path,
+                                       reader='manual',
+                                       skip_lines=[2, 0])
+    molecules = reader.read()
+    assert len(molecules) == 2
+    assert len(molecules[1]['mol']) == 20
 
-class TestXYZreader(unittest.TestCase):
-    def test_string(self):
-        reader= XYZreader(path_pattern='[2-3]/*.opt.xyz',
-                                           path_root = DATA_PATH,
-                                           reader='manual',
-                                           skip_lines=[0, 0])
-        molecules = reader.read()
-        self.assertEqual(len(molecules) , 2)
-        self.assertEqual(len(molecules[1]['mol']), 38)
-
-    def test_list(self):
-        reader = XYZreader(path_pattern = ['[2-3]/*.opt.xyz', '[1-2][1-2]/*.opt.xyz'],
-                                           path_root = DATA_PATH,
-                                           reader='manual',
-                                           skip_lines = [0, 0])
-        molecules = reader.read()
-        self.assertEqual(len(molecules) , 4)
-        self.assertEqual(len(molecules[1]['mol']), 38)
-        self.assertEqual(len(molecules[4]['mol']), 41)
-
-
-if __name__== '__main__':
-    unittest.main()
-
-
+def test_list_manual(data_path):
+    reader = XYZreader(path_pattern = ['[2-3]_opt.xyz', '[1-2][1-2]_opt.xyz'],
+                                       path_root = data_path,
+                                       reader='manual',
+                                       skip_lines = [2, 0])
+    molecules = reader.read()
+    assert len(molecules) == 6
+    assert len(molecules[1]['mol']) == 20
+    assert len(molecules[4]['mol']) == 24
 
--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -1,53 +1,56 @@
-import unittest
+import pytest
 import os
 import shutil, tempfile
 import numpy as np
 import pandas as pd
-from cheml.visualization import scatter2D, hist
-from cheml.visualization import decorator
-from cheml.visualization import SavePlot
-
-# dummy data
-x = pd.DataFrame(np.arange(0.0, 1.0, 0.01))
-y = pd.DataFrame(np.sin(2*np.pi*x))
+from chemml.visualization import scatter2D, hist
+from chemml.visualization import decorator
+from chemml.visualization import SavePlot
 
 
+@pytest.fixture()
+def dummy_data():
+    x = pd.DataFrame(np.arange(0.0, 1.0, 0.01))
+    y = pd.DataFrame(np.sin(2 * np.pi * x))
+    return (x,y)
 
-class Testvisualization(unittest.TestCase):
-    def setUp(self):
-        # Create a temporary directory
-        self.test_dir = tempfile.mkdtemp()
+@pytest.fixture()
+def setup_teardown():
+    # Create a temporary directory
+    test_dir = tempfile.mkdtemp()
+    # return test directory to save figures
+    yield test_dir
+    # Remove the directory after the test
+    shutil.rmtree(test_dir)
 
-    def tearDown(self):
-        # Remove the directory after the test
-        shutil.rmtree(self.test_dir)
+def test_scatter2D(dummy_data):
+    sc = scatter2D('r', linestyle='--')
+    x, y = dummy_data
+    fig = sc.plot(x,y,0,0)
+    # fig.show()
 
-    def test_scatter2D(self):
-        sc = scatter2D('r', linestyle='--')
-        fig = sc.plot(x,y,0,0)
-        fig.show()
+def test_hist(dummy_data):
+    hg = hist(20,'g',{'density':True})
+    x, y = dummy_data
+    fig = hg.plot(y,0)
+    # fig.show()
 
-    def test_hist(self):
-        hg = hist(20,'g',{'normed':True})
-        fig = hg.plot(y,0)
-        fig.show()
+def test_decorator(dummy_data):
+    hg = hist(20,'g',{'density':True})
+    x, y = dummy_data
+    fig = hg.plot(y,0)
+    dec = decorator('histogram',xlabel='sin', ylabel='sin%', xlim=(4,None), ylim=(0,None),
+            grid=True, grid_color='g', grid_linestyle=':', grid_linewidth=0.5)
+    fig = dec.fit(fig)
+    dec.matplotlib_font()
+    # fig.show()
 
-    def test_decorator(self):
-        hg = hist(20,'g',{'normed':True})
-        fig = hg.plot(y,0)
-        dec = decorator('histogram',xlabel='sin', ylabel='sin%', xlim=(4,None), ylim=(0,None),
-                grid=True, grid_color='g', grid_linestyle=':', grid_linewidth=0.5)
-        fig = dec.fit(fig)
-        dec.matplotlib_font()
-        fig.show()
-
-    def test_SavePlot(self):
-        sc = scatter2D('r', linestyle='--')
-        fig = sc.plot(x,y,0,0)
-        sp = SavePlot('Sin', os.path.join(self.test_dir,'plots'), 'eps', {'facecolor': 'w', 'dpi': 100, 'pad_inches': 0.1, 'bbox_inches': 'tight'})
-        sp.save(fig)
+def test_SavePlot(dummy_data,setup_teardown):
+    sc = scatter2D('r', linestyle='--')
+    x, y = dummy_data
+    fig = sc.plot(x,y,0,0)
+    sp = SavePlot('Sin', os.path.join(setup_teardown,'plots'), 'eps', {'facecolor': 'w', 'dpi': 100, 'pad_inches': 0.1, 'bbox_inches': 'tight'})
+    sp.save(fig)
 
 
-if __name__== '__main__':
-    unittest.main()
 

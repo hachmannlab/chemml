ac62881eea9985bf68a604f4c64853aa09a83350
mojtaba <mojtabah@buffalo.edu>
2018-09-28 12:59:05 -0700

    tested genetic algorithm code: check for warnings

--- tests/test_GA_DEAP.py
+++ tests/test_GA_DEAP.py
@@ -1,6 +1,6 @@
 import unittest
 
-from cheml.search import GA_DEAP
+from chemml.search import GeneticAlgorithm
 
 Weights = (1,)
 chromosome_length = 2
@@ -9,41 +9,36 @@ bit_limits = ( (0,10), (0,5) )
 mut_int_lower = (0,0)
 mut_int_upper = (10,5)
 
-def Evaluate(individual):
+
+def evaluate(individual):
     return sum(individual),
 
 
-class TestGA_DEAP(unittest.TestCase):
-    def test_algorithm1(self):
+class TestGeneticAlgorithm(unittest.TestCase):
 
-        ga_search = GA_DEAP(Evaluate, Weights=Weights, chromosome_length = chromosome_length, chromosome_type = chromosome_type,
-                       bit_limits = bit_limits, mut_int_lower=mut_int_lower, mut_int_upper=mut_int_upper )
+    def test_algorithm1(self):
+        ga_search = GeneticAlgorithm(evaluate, weights=Weights, chromosome_length = chromosome_length, chromosome_type = chromosome_type,
+                       bit_limits = bit_limits, mut_int_lower=mut_int_lower, mut_int_upper=mut_int_upper, algorithm=1)
 
         ga_search.fit()
-        best_ind_df, best_individual = ga_search.algorithm_1()
+        best_ind_df, best_individual = ga_search.search()
         self.assertEqual(15, sum(best_individual))
 
     def test_algorithm2(self):
-        ga_search = GA_DEAP(Evaluate, Weights=Weights, chromosome_length=chromosome_length, chromosome_type=chromosome_type,
-                            bit_limits=bit_limits, mut_int_lower=mut_int_lower, mut_int_upper=mut_int_upper)
+        ga_search = GeneticAlgorithm(evaluate, weights=Weights, chromosome_length=chromosome_length, chromosome_type=chromosome_type,
+                            bit_limits=bit_limits, mut_int_lower=mut_int_lower, mut_int_upper=mut_int_upper, algorithm=2)
         ga_search.fit()
-        best_ind_df, best_individual = ga_search.algorithm_2()
+        best_ind_df, best_individual = ga_search.search()
         self.assertEqual(15, sum(best_individual))
 
     def test_algorithm3(self):
-        ga_search = GA_DEAP(Evaluate, Weights=Weights, chromosome_length=chromosome_length, chromosome_type=chromosome_type,
-                            bit_limits=bit_limits, mut_int_lower=mut_int_lower, mut_int_upper=mut_int_upper)
+        ga_search = GeneticAlgorithm(evaluate, weights=Weights, chromosome_length=chromosome_length, chromosome_type=chromosome_type,
+                            bit_limits=bit_limits, mut_int_lower=mut_int_lower, mut_int_upper=mut_int_upper, algorithm=3)
 
         ga_search.fit()
-        best_ind_df, best_individual = ga_search.algorithm_3()
-        self.assertEqual(15, sum(best_individual))
-
-    def test_algorithm4(self):
-        ga_search = GA_DEAP(Evaluate, Weights=Weights, chromosome_length=chromosome_length, chromosome_type=chromosome_type,
-                            bit_limits=bit_limits, mut_int_lower=mut_int_lower, mut_int_upper=mut_int_upper)
-
-        ga_search.fit()
-        best_ind_df, best_individual = ga_search.algorithm_4()
+        best_ind_df, best_individual = ga_search.search()
         self.assertEqual(15, sum(best_individual))
 
 
+if __name__== '__main__':
+    unittest.main()

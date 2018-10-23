eab852c4401854104a35de66be61fd05b515f8d4
mojtaba <mojtabah@buffalo.edu>
2018-10-22 20:43:47 -0700

    retrieved the correct test file

--- tests/utils/test_validation.py
+++ tests/utils/test_validation.py
@@ -1,29 +1,67 @@
+import numpy as np
+import pandas as pd
 import pytest
 import os
 import pkg_resources
 
-from chemml.initialization import XYZreader
+from chemml.utils import check_object_col
+from chemml.utils import isfloat
+from chemml.utils import islist
+from chemml.utils import istuple
+from chemml.utils import isnpdot
+from chemml.utils import isint
+from chemml.utils import value
 
 
 @pytest.fixture()
 def data_path():
-    return pkg_resources.resource_filename('chemml', os.path.join('datasets', 'data', 'organic_xyz'))
+    return pkg_resources.resource_filename('chemml', os.path.join('datasets', 'data', 'test_files'))
 
 
-def test_string_manual(data_path):
-    reader = XYZreader(
-        path_pattern='[2-3]_opt.xyz', path_root=data_path, reader='manual', skip_lines=[2, 0])
-    molecules = reader.read()
-    assert len(molecules) == 2
-    assert len(molecules[1]['mol']) == 20
+def test_isfloat_exception():
+    assert isfloat('1') is True
+    assert isfloat('a') is False
 
 
-def test_list_manual(data_path):
-    reader = XYZreader(
-        path_pattern=['[2-3]_opt.xyz', '[1-2][1-2]_opt.xyz'],
-        path_root=data_path,
-        reader='manual',
-        skip_lines=[2, 0])
-    molecules = reader.read()
-    assert len(molecules) == 6
-    assert len(molecules[1]['mol']) == 20
+def test_islist_exception():
+    assert islist('[1]') is True
+    assert islist([1]) is True
+    assert islist('a') is False
+
+
+def test_istuple_exception():
+    assert istuple('(1,)') is True
+    assert istuple((1,)) is True
+    assert istuple('a') is False
+
+
+def test_isnpdot_exception():
+    assert isnpdot('np.') is True
+    with pytest.raises(ValueError):
+        assert isnpdot(np.sin) is True
+
+
+def test_isint_exception():
+    assert isint('1') is True
+    assert isint(1) is True
+    assert isint('a') is False
+
+
+def test_value():
+    assert value('1') == 1
+    assert value('np.sin') == np.sin
+    assert value('type') == 'type'
+
+
+def test_check_object_col_exception(data_path):
+    with pytest.raises(ValueError):
+        df = pd.read_csv(os.path.join(data_path, 'test_missing_values.csv'), header=None)
+        f = check_object_col(df, 'df')
+
+
+def test_check_object_col():
+    df = pd.DataFrame()
+    df[0] = ['a', 'b', 'c']
+    df[1] = [1, 2, 3]
+    f = check_object_col(df, 'df')
+

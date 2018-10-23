78a201bb5774f6072e02e243ce3ab9c5f993a8aa
mojtaba <mojtabah@buffalo.edu>
2018-10-22 20:11:19 -0700

    test and clean initialization

--- chemml/initialization/__init__.py
+++ chemml/initialization/__init__.py
@@ -3,23 +3,17 @@ The cheml.initialization module includes (please click on links adjacent to func
     - XYZreader: :func:`~cheml.initialization.XYZreader`
     - ConvertFile: :func:`~cheml.initialization.ConvertFile`
     - Split: :func:`~cheml.initialization.Split`
-    - SaveFile: :func:`~cheml.initialization.SaveFile`
 
 """
 
 from .initialization import Split
 
 from .initialization import XYZreader
-from .initialization import SaveFile
-# from .initialization import StoreFile
 
 from .initialization import ConvertFile
-# from .data import Trimmer
-# from .data import Uniformer
 
 __all__ = [
     'XYZreader',
     'ConvertFile',
     'Split',
-    'SaveFile',
 ]
--- tests/initialization/test_XYZreader.py
+++ tests/initialization/test_XYZreader.py
@@ -7,12 +7,16 @@ from chemml.initialization import XYZreader
 
 @pytest.fixture()
 def data_path():
-    return pkg_resources.resource_filename('chemml', os.path.join('datasets', 'data', 'organic_xyz'))
+    return pkg_resources.resource_filename(
+        'chemml', os.path.join('datasets', 'data', 'organic_xyz'))
 
 
 def test_string_manual(data_path):
     reader = XYZreader(
-        path_pattern='[2-3]_opt.xyz', path_root=data_path, reader='manual', skip_lines=[2, 0])
+        path_pattern='[2-3]_opt.xyz',
+        path_root=data_path,
+        reader='manual',
+        skip_lines=[2, 0])
     molecules = reader.read()
     assert len(molecules) == 2
     assert len(molecules[1]['mol']) == 20
@@ -27,3 +31,15 @@ def test_list_manual(data_path):
     molecules = reader.read()
     assert len(molecules) == 6
     assert len(molecules[1]['mol']) == 20
+
+
+def test_extension_exception(data_path):
+    with pytest.raises(ValueError):
+        reader = XYZreader('[2-3]_opt')
+        reader.read()
+
+
+def test_notxyz_exception(data_path):
+    with pytest.raises(ValueError):
+        reader = XYZreader('[2-3].opt')
+        reader.read()

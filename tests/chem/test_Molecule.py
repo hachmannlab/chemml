c195f8e30e17503b2863a3485ffe15f32271b66a
mojtaba <mojtabah@buffalo.edu>
2019-02-21 22:49:22 -0500

    added 19 tests for Molecule class

--- tests/chem/test_Molecule.py
+++ tests/chem/test_Molecule.py
@@ -1,67 +1,217 @@
-import unittest
-import numpy as np
-from chemml.chem import CoulombMatrix
+import pytest
+from rdkit import Chem
+import warnings
 
-# Oxygen, Hydrogen, Hydrogen
-n = np.array([8, 1, 1])
-c = np.array([[1.464, 0.707, 1.056], [0.878, 1.218, 0.498], [2.319, 1.126, 0.952]])
-nc = n.reshape((3, 1))
-nc = np.append(nc, c, 1)
+from chemml.chem import Molecule
 
 
-class TestCoulombMatrix(unittest.TestCase):
-    def test_UM(self):
-        cm = CoulombMatrix('UM')
-        h2o = cm.represent(np.array([nc]))
+@pytest.fixture()
+def caffeine_smiles():
+    smiles = 'CN1C=NC2=C1C(=O)N(C(=O)N2C)C'
+    return smiles
 
-        self.assertEqual(h2o.shape, (1, cm.max_n_atoms**2))
-        a = np.array([[
-            73.51669472, 8.3593106, 8.35237809, 8.3593106, 0.5, 0.66066557, 8.35237809, 0.66066557, 0.5
-        ]])
-        self.assertAlmostEqual(a[0][0], h2o.values[0][0])
-        self.assertAlmostEqual(a[0][1], h2o.values[0][1])
-        self.assertAlmostEqual(a[0][-1], h2o.values[0][-1])
+@pytest.fixture()
+def caffeine_canonical():
+    smiles = 'Cn1c(=O)c2c(ncn2C)n(C)c1=O'
+    return smiles
 
-    def test_UT(self):
-        cm = CoulombMatrix('UT')
-        h2o = cm.represent(np.array([nc]))
+@pytest.fixture()
+def caffeine_kekulize():
+    smiles = 'CN1C(=O)C2=C(N=CN2C)N(C)C1=O'
+    return smiles
 
-        self.assertEqual(h2o.shape, (1, cm.max_n_atoms * (cm.max_n_atoms + 1) / 2))
-        a = np.array([[73.51669472, 8.3593106, 0.5, 8.35237809, 0.66066557, 0.5]])
-        self.assertAlmostEqual(a[0][0], h2o.values[0][0])
-        self.assertAlmostEqual(a[0][1], h2o.values[0][1])
-        self.assertAlmostEqual(a[0][-1], h2o.values[0][-1])
+@pytest.fixture()
+def caffeine_smarts():
+    smarts = '[#6]-[#7]1:[#6]:[#7]:[#6]2:[#6]:1:[#6](=[#8]):[#7](:[#6](=[#8]):[#7]:2-[#6])-[#6]'
+    return smarts
 
-    def test_E(self):
-        cm = CoulombMatrix('E')
-        h2o = cm.represent(np.array([nc]))
+@pytest.fixture()
+def caffeine_inchi():
+    inchi = 'InChI=1S/C8H10N4O2/c1-10-4-9-6-5(10)7(13)12(3)8(14)11(6)2/h4H,1-3H3'
+    return inchi
 
-        self.assertEqual(h2o.shape, (1, cm.max_n_atoms))
-        a = np.array([[75.39770052, -0.16066482, -0.72034098]])
-        self.assertAlmostEqual(a[0][0], h2o.values[0][0])
-        self.assertAlmostEqual(a[0][1], h2o.values[0][1])
-        self.assertAlmostEqual(a[0][-1], h2o.values[0][-1])
+def test_exception_smiles():
+    m = Molecule()
+    # syntax error: SMILES Parse Error
+    with pytest.raises(ValueError):
+        m.load('fake', 'smiles')
+    # wrong SMILES
+    with pytest.raises(ValueError):
+        m.load('CO(C)C', 'smiles')
+    # can't kekulize
+    with pytest.raises(ValueError):
+        m.load('c1cc1', 'smiles')
 
-    def test_SC(self):
-        cm = CoulombMatrix('SC')
-        h2o = cm.represent(np.array([nc]))
+def test_exception_smarts():
+    m = Molecule()
+    # syntax error: SMARTS Parse Error
+    with pytest.raises(ValueError):
+        m.load('fake', 'smarts')
+    # syntax error: SMARTS Parse Error
+    with pytest.raises(ValueError):
+        m.load('[#6](=[#6]', 'smarts')
 
-        self.assertEqual(h2o.shape, (1, cm.max_n_atoms * (cm.max_n_atoms + 1) / 2))
-        a = np.array([[73.51669472, 8.3593106, 0.5, 8.35237809, 0.66066557, 0.5]])
-        self.assertAlmostEqual(a[0][0], h2o.values[0][0])
-        self.assertAlmostEqual(a[0][1], h2o.values[0][1])
-        self.assertAlmostEqual(a[0][-1], h2o.values[0][-1])
+def test_exception_inchi():
+    m = Molecule()
+    # syntax error: SMARTS Parse Error
+    with pytest.raises(ValueError):
+        m.load('fake', 'inchi')
+    # inchi key: syntax error: SMARTS Parse Error
+    with pytest.raises(ValueError):
+        m.load('RYYVLZVUVIJVGH-UHFFFAOYSA-N', 'inchi')
 
-    def test_RC(self):
-        cm = CoulombMatrix('RC')
-        h2o = cm.represent(np.array([nc]))
+def test_load_smiles(caffeine_smiles):
+    m = Molecule()
+    m.load(caffeine_smiles, 'smiles')
+    assert m.creator == ('SMILES', caffeine_smiles)
+    assert isinstance(m.rdkit_molecule, Chem.Mol)
 
-        self.assertEqual(h2o.shape, (1, cm.nPerm * cm.max_n_atoms * (cm.max_n_atoms + 1) / 2))
-        a = np.array([[
-            0.5, 8.35237809, 73.51669472, 0.66066557, 8.3593106, 0.5, 73.51669472, 8.35237809, 0.5,
-            8.3593106, 0.66066557, 0.5, 0.5, 8.3593106, 73.51669472, 0.66066557, 8.35237809, 0.5
-        ]])
+def test_reload_warning(caffeine_smiles):
+    m = Molecule()
+    m.load(caffeine_smiles, 'smiles')
+    warnings.simplefilter("always")
+    with warnings.catch_warnings(record=True) as w:
+        m.load(caffeine_smiles, 'smiles')
 
+def test_load_smarts(caffeine_smarts):
+    m = Molecule()
+    m.load(caffeine_smarts, 'smarts')
+    assert m.creator == ('SMARTS', caffeine_smarts)
+    assert isinstance(m.rdkit_molecule, Chem.Mol)
 
-if __name__ == '__main__':
-    unittest.main()
+def test_load_inchi(caffeine_inchi):
+    m = Molecule()
+    m.load(caffeine_inchi, 'inchi')
+    assert m.creator == ('InChi', caffeine_inchi)
+    assert isinstance(m.rdkit_molecule, Chem.Mol)
+
+def test_mol2smiles(caffeine_smiles, caffeine_canonical, caffeine_kekulize):
+    m = Molecule()
+    m.load(caffeine_smiles, 'smiles')
+    m.to_smiles()
+    assert isinstance(m.rdkit_molecule, Chem.Mol)
+    assert m.smiles == caffeine_canonical
+    m.to_smiles(kekuleSmiles=True)
+    assert m.smiles == caffeine_kekulize
+    # kekulize takes priority over canonical
+    m.to_smiles(kekuleSmiles=True)
+    assert m.smiles == caffeine_kekulize
+    # if kekule is False, everything is OK
+    m.to_smiles(canonical=True)
+    assert m.smiles == caffeine_canonical
+
+def test_mol2smiles_defaultargs(caffeine_smiles):
+    m = Molecule()
+    m.load(caffeine_smiles, 'smiles')
+    m.to_smiles()
+    m.to_smiles(**m._default_smiles_args)
+    # all the args & canonical=False & doRandom=True
+    args = m._default_smiles_args
+    args['canonical'] = False
+    args['doRandom'] = True
+    m.to_smiles(**args)
+    assert m.smiles_args['canonical'] is False
+    assert m.smiles_args['doRandom'] is True
+
+def test_mol2smarts(caffeine_smarts):
+    m = Molecule()
+    m.load(caffeine_smarts, 'smarts')
+    m.to_smarts()
+    assert isinstance(m.rdkit_molecule, Chem.Mol)
+    assert m.smarts == caffeine_smarts
+    m.to_smarts(isomericSmiles=False)
+    assert m.smarts == caffeine_smarts
+
+def test_mol2smarts_defaultargs(caffeine_smarts):
+    m = Molecule()
+    m.load(caffeine_smarts, 'smarts')
+    m.to_smarts()
+    m.to_smarts(**m._default_smarts_args)
+    # all the args & isomericSmiles=False
+    args = m._default_smarts_args
+    args['isomericSmiles'] = False
+    m.to_smarts(**args)
+    assert m.smarts_args['isomericSmiles'] is False
+
+def test_mol2inchi(caffeine_inchi):
+    m = Molecule()
+    m.load(caffeine_inchi, 'inchi')
+    m.to_inchi()
+    assert isinstance(m.rdkit_molecule, Chem.Mol)
+    assert m.inchi == caffeine_inchi
+
+def test_mol2inchi_defaultargs(caffeine_inchi):
+    m = Molecule()
+    m.load(caffeine_inchi, 'inchi')
+    m.to_inchi()
+    m.to_inchi(**m._default_inchi_args)
+    # all the args & treatWarningAsError=True
+    args = m._default_inchi_args
+    args['treatWarningAsError'] = True
+    m.to_inchi(**args)
+    assert m.inchi_args['treatWarningAsError'] is True
+
+def test_hydrogens(caffeine_smiles, caffeine_canonical, caffeine_inchi):
+    m = Molecule()
+    m.load(caffeine_smiles, 'smiles')
+    m.to_smiles()
+    assert isinstance(m.rdkit_molecule, Chem.Mol)
+    assert m.smiles == caffeine_canonical
+    # add hydrogens
+    m.hydrogens('add', explicitOnly=False)
+    # canonical smiles with hydrogens
+    m.to_smiles()
+    assert m.smiles == "[H]c1nc2c(c(=O)n(C([H])([H])[H])c(=O)n2C([H])([H])[H])n1C([H])([H])[H]"
+    # test inchi
+    m = Molecule()
+    m.load(caffeine_inchi, 'inchi')
+    m.to_inchi()
+    assert m.inchi == caffeine_inchi
+    # add hydrogens
+    m.hydrogens('add')
+    m.to_inchi()
+    assert m.inchi == caffeine_inchi
+
+def test_hydrogens_exception(caffeine_smiles):
+    m = Molecule()
+    m.load(caffeine_smiles, 'smiles')
+    with pytest.raises(ValueError):
+        m.hydrogens('addHs')
+
+def test_check_rdkit_molecule_exception():
+    m = Molecule()
+    with pytest.raises(ValueError):
+        m.hydrogens('addHs')
+
+def test_mol2xyz_exception(caffeine_smiles):
+    m = Molecule()
+    m.load(caffeine_smiles, 'smiles')
+    m.hydrogens('add')
+    with pytest.raises(ValueError):
+        m.to_xyz()
+    with pytest.raises(ValueError):
+        m.to_xyz('uff')
+
+def test_mol2xyz_MMFF(caffeine_smiles):
+    m = Molecule()
+    m.load(caffeine_smiles, 'smiles')
+    m.hydrogens('add')
+    m.to_xyz('MMFF', maxIters=210)
+    assert m.xyz.geometry.shape[0] == m.rdkit_molecule.GetNumAtoms()
+    m.to_xyz('MMFF', maxIters=110, mmffVariant='MMFF94s')
+    assert m.xyz.atomic_symbols.shape[0] == m.rdkit_molecule.GetNumAtoms()
+    assert m.MMFF_args['mmffVariant'] == 'MMFF94s'
+    # check default args
+    m.to_xyz('MMFF', **m._default_MMFF_args)
+
+def test_mol2xyz_UFF(caffeine_smiles):
+    m = Molecule()
+    m.load(caffeine_smiles, 'smiles')
+    m.hydrogens('add')
+    m.to_xyz('UFF', ignoreInterfragInteractions=False)
+    assert m.xyz.geometry.shape[0] == m.rdkit_molecule.GetNumAtoms()
+    m.to_xyz('UFF', maxIters=110)
+    assert m.xyz.atomic_symbols.shape[0] == m.rdkit_molecule.GetNumAtoms()
+    assert m.UFF_args['maxIters'] == 110
+    # check default args
+    m.to_xyz('UFF', **m._default_UFF_args)

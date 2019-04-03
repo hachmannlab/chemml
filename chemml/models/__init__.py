7e62d68da465437574c0ba635e867a78022ad15f
mojtaba <mojtabah@buffalo.edu>
2019-04-03 09:06:59 -0400

    updated docs links

--- chemml/models/__init__.py
+++ chemml/models/__init__.py
@@ -1,11 +1,14 @@
 """
-The 'chemml.models' module includes trained or untrained ML models.
+The 'chemml.models' module includes (please click on links adjacent to function names for more information):
+    - OrganicLorentzLorenz: :func:`~chemml.models.keras.trained.OrganicLorentzLorenz`
+    - MLP: :func:`~chemml.models.keras.mlp.MLP`
 """
 
-
-
+from chemml.models.keras.mlp import MLP
+from chemml.models.keras.trained import OrganicLorentzLorenz
 
 
 __all__ = [
-
+    'OrganicLorentzLorenz',
+    'MLP'
 ]
--- chemml/models/keras/__init__.py
+++ chemml/models/keras/__init__.py
@@ -1,7 +1,5 @@
 """
 The chemml.models.keras module includes (please click on links adjacent to function names for more information):
-    - MLP: :func:`~chemml.nn.keras.MLP`
-    - MLP_sklearn: :func:`~chemml.nn.keras.MLP_sklearn`
 """
 
 

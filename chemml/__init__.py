2b136fc79cb5d8fbff89d39ca7ca82999d2477f1
mojtaba <mojtabah@buffalo.edu>
2019-07-14 17:10:17 -0400

    not removing bytecodes anymore

--- chemml/__init__.py
+++ chemml/__init__.py
@@ -3,5 +3,5 @@ __version__ = "0.5.2"
 __author__ = ["Mojtaba Haghighatlari (mojtabah@buffalo.edu)", "Johannes Hachmann (hachmann@buffalo.edu)"]
 
 
-import sys
-sys.dont_write_bytecode = True
+# import sys
+# sys.dont_write_bytecode = True

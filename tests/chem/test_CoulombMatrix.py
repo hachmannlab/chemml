24f98fc2a5f4e2f1cf07022a88107516ff850750
mojtaba <mojtabah@buffalo.edu>
2019-08-21 11:32:44 -0400

    changed name of attributes

--- tests/chem/test_CoulombMatrix.py
+++ tests/chem/test_CoulombMatrix.py
@@ -34,7 +34,7 @@ def test_exception(mols):
 def test_UM(mols):
     cm = CoulombMatrix('UM')
     h2o = cm.represent(mols)
-    assert h2o.shape == (1, cm.max_n_atoms**2)
+    assert h2o.shape == (1, cm.max_n_atoms_ ** 2)
     a = np.array([[
         73.51669472, 8.3593106, 8.35237809, 8.3593106, 0.5, 0.66066557, 8.35237809, 0.66066557, 0.5
     ]])
@@ -46,7 +46,7 @@ def test_UT(mols):
     cm = CoulombMatrix('UT')
     h2o = cm.represent(mols )
 
-    assert h2o.shape == (1, cm.max_n_atoms * (cm.max_n_atoms + 1) / 2)
+    assert h2o.shape == (1, cm.max_n_atoms_ * (cm.max_n_atoms_ + 1) / 2)
     a = np.array([[73.51669472, 8.3593106, 0.5, 8.35237809, 0.66066557, 0.5]])
     assert a[0][0] == pytest.approx(h2o.values[0][0])
     assert a[0][1] == pytest.approx(h2o.values[0][1])
@@ -56,7 +56,7 @@ def test_E(mols):
     cm = CoulombMatrix('E')
     h2o = cm.represent(mols )
 
-    assert h2o.shape == (1, cm.max_n_atoms)
+    assert h2o.shape == (1, cm.max_n_atoms_)
     a = np.array([[75.39770052, -0.16066482, -0.72034098]])
     assert a[0][0] == pytest.approx( h2o.values[0][0])
     assert a[0][1] == pytest.approx( h2o.values[0][1])
@@ -66,7 +66,7 @@ def test_SC(mols):
     cm = CoulombMatrix('SC')
     h2o = cm.represent(mols )
 
-    assert h2o.shape == (1, cm.max_n_atoms * (cm.max_n_atoms + 1) / 2)
+    assert h2o.shape == (1, cm.max_n_atoms_ * (cm.max_n_atoms_ + 1) / 2)
     a = np.array([[73.51669472, 8.3593106, 0.5, 8.35237809, 0.66066557, 0.5]])
     assert a[0][0] == pytest.approx( h2o.values[0][0])
     assert a[0][1] == pytest.approx( h2o.values[0][1])
@@ -76,7 +76,7 @@ def test_RC(mols):
     cm = CoulombMatrix('RC')
     h2o = cm.represent(mols )
 
-    assert h2o.shape == (1, cm.nPerm * cm.max_n_atoms * (cm.max_n_atoms + 1) / 2)
+    assert h2o.shape == (1, cm.nPerm * cm.max_n_atoms_ * (cm.max_n_atoms_ + 1) / 2)
     a = np.array([[
         0.5, 8.35237809, 73.51669472, 0.66066557, 8.3593106, 0.5, 73.51669472, 8.35237809, 0.5,
         8.3593106, 0.66066557, 0.5, 0.5, 8.3593106, 73.51669472, 0.66066557, 8.35237809, 0.5

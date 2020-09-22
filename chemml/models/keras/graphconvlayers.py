ca5359b81b5a07abe22e3fc4d5a7730fc2a7b3fa
aditya1707 <adityaso@buffalo.edu>
2020-09-22 12:06:57 -0400

    temporary fix for version issues

--- chemml/models/keras/graphconvlayers.py
+++ chemml/models/keras/graphconvlayers.py
@@ -1,9 +1,12 @@
 import tensorflow as tf
-import tensorflow.keras.bakend as k
+#import tensorflow.keras.backend as k
+from keras.backend as K
+# from tensorflow.keras import layers
+# from tensorflow.keras.layers import deserialize as layer_from_config
+from keras import layers
+from keras.layers import deserialize as layer_from_config
 
-from tensorflow.keras import layers
-from tensorflow.keras.layers import deserialize as layer_from_config
-from chemml.utils import mol_shapes_to_dims
+from chemml.utils.utilities import mol_shapes_to_dims
 
 class NeuralGraphHidden(layers.layer):
     
